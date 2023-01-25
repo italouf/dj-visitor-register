3ae360531fed74976b7644e1f100afbb29ebcbf2
Italo <ItaloSilva.thera@suzano.com.br>
2023-01-25 15:14:25 -0300

    template inserido

--- controle_visitantes/controle_visitantes/settings.py
+++ controle_visitantes/controle_visitantes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'controle_visitantes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- controle_visitantes/usuarios/views.py
+++ controle_visitantes/usuarios/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World")
 
-# Create your views here.
+    context = {
+        "nome_pagina": "Dashboard Inicial",
+    }
+
+    return render(request, "index.html", context)
+
