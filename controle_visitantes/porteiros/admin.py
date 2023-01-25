577bbe0ec9f89a18b43e1bc5cf2d849e2f7b2a85
Italo <ItaloSilva.thera@suzano.com.br>
2023-01-25 14:23:36 -0300

    app-porteiros-finalizado

--- controle_visitantes/porteiros/admin.py
+++ controle_visitantes/porteiros/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from porteiros.models import Porteiro
 
+admin.site.register(Porteiro)
 # Register your models here.
--- controle_visitantes/porteiros/models.py
+++ controle_visitantes/porteiros/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Porteiro(models.Model):
+
+    usuario = models.OneToOneField(
+        "usuarios.Usuario",
+        verbose_name="Usuário",
+        on_delete=models.PROTECT
+    )
+    
+    nome_completo = models.CharField(
+        verbose_name="Nome completo",
+        max_length=128,
+    )
+
+    cpf = models.CharField(
+        verbose_name="CPF",
+        max_length=11,
+    )
+
+    telefone = models.CharField(
+        verbose_name="Telefone para contato",
+        max_length=11,
+    )
+
+    data_nascimento = models.DateField(
+        verbose_name="Data de nascimento",
+        auto_now_add=False,
+        auto_now=False,
+    )
+
+    class Meta:
+        verbose_name = "Porteiro"
+        verbose_name_plural = "Porteiros"
+        db_table = "porteiro"
+    
+    def __str__(self):
+        return self.nome_completo
