8579d5dde756b4b033de8ae8f53b04d90a0c6db2
Italo <ItaloSilva.thera@suzano.com.br>
2023-01-25 10:37:45 -0300

    app-usuarios-final

--- controle_visitantes/usuarios/models.py
+++ controle_visitantes/usuarios/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin,)
 
-# Create your models here.
+class UsuarioManager(BaseUserManager):
+
+    def create_user(self, email, password=None):
+        usuario = self.model(email=self.normalize_email(email))
+
+        usuario.is_active = True
+        usuario.is_staff = False
+        usuario.is_superuser = False
+
+        if password:
+            usuario.set_password(password)
+        
+        usuario.save()
+        return usuario
+
+    def create_superuser(self, email, password=None):
+        usuario = self.create_user(email=self.normalize_email(email), password=password,)
+
+        usuario.is_active = True
+        usuario.is_staff = True
+        usuario.is_superuser = True
+
+        usuario.set_password(password)
+        usuario.save()
+
+        return usuario
+
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(verbose_name=("Email"), max_length= 128, unique=True)
+
+    is_active = models.BooleanField(verbose_name=("Ativo"), default=True)
+
+    is_staff = models.BooleanField(verbose_name=("Equipe de gerenciamento"), default=False)
+
+    is_superuser = models.BooleanField(verbose_name=("Super usuário"), default=False)
+
+    USERNAME_FIELD = "email"
+
+    objects = UsuarioManager()
+
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+        db_table = "usuario"
+    
+    def __str__(self):
+        return self.email
