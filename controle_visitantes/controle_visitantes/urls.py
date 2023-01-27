a0f6edf87095b83debc6ca89924f7fd9dccd9a95
Italo <ItaloSilva.thera@suzano.com.br>
2023-01-27 10:56:36 -0300

    view registro de visitante e url

--- controle_visitantes/controle_visitantes/urls.py
+++ controle_visitantes/controle_visitantes/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from usuarios.views import index
+from visitantes.views import registrar_visitante
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index, name="index"),
+    path("registrar-visitante/", registrar_visitante, name="registrar-visitante"),
 ]
--- controle_visitantes/visitantes/views.py
+++ controle_visitantes/visitantes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def registrar_visitante(request):
+    
+    context = {}
+
+    return render(request, "registrar_visitante.html", context)
