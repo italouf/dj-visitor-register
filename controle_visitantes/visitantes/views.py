6a633e03e2d0c95391bbc83203a77b251f507c28
Italo <ItaloSilva.thera@suzano.com.br>
2023-01-27 16:03:12 -0300

    registrar visitante pelo template ok

--- controle_visitantes/visitantes/views.py
+++ controle_visitantes/visitantes/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from visitantes.forms import VisitanteForm
 
 def registrar_visitante(request):
     
     form = VisitanteForm()
+
+    if request.method == 'POST':
+        form = VisitanteForm(request.POST)
+
+        if form.is_valid():
+            visitante = form.save(commit=False)
+
+            visitante.registrado_por = request.user.porteiro
+            visitante.save()
+
+            return redirect("index")
+
 
     context = {
         "nome_pagina": "Registrar Visitante",
