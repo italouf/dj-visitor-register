871f1e12c64f5389c9c62cfeeb1d0f8b267df3b2
Italo <ItaloSilva.thera@suzano.com.br>
2023-01-27 17:01:11 -0300

    insercao-template-informacoes-visitante

--- controle_visitantes/visitantes/views.py
+++ controle_visitantes/visitantes/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from visitantes.models import Visitante
 from visitantes.forms import VisitanteForm
 from django.contrib import messages
+
 
 def registrar_visitante(request):
     
@@ -29,3 +31,14 @@ def registrar_visitante(request):
     }
 
     return render(request, "registrar_visitante.html", context)
+
+def informacoes_visitante(request, id):
+
+    visitante = get_object_or_404(Visitante, id=id)
+
+    context = {
+        "nome_pagina": "Informações de visitante",
+        "visitante": visitante,
+    }
+
+    return render(request, "informacoes_visitante.html", context)
