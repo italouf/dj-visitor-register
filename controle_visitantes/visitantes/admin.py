8b6e4118085c93dbcdda119ca013291e79d39ebc
Italo <ItaloSilva.thera@suzano.com.br>
2023-01-27 10:33:11 -0300

    modelo-app-visitantes-completo

--- controle_visitantes/visitantes/admin.py
+++ controle_visitantes/visitantes/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from visitantes.models import Visitante
 
-# Register your models here.
+admin.site.register(Visitante)
--- controle_visitantes/visitantes/models.py
+++ controle_visitantes/visitantes/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+class Visitante(models.Model):
+    nome_completo = models.CharField(
+        verbose_name="Nome completo",
+        max_length=128,)
+
+    cpf = models.CharField(
+        verbose_name="CPF",
+        max_length=11,
+    )
+
+    data_nascimento = models.DateField(
+        verbose_name="Data de Nascimento",
+        auto_now_add=False,
+        auto_now = False,
+    )
+
+    numero_casa = models.PositiveSmallIntegerField(
+        verbose_name = "Numero da Casa",
+    )
+
+    placa_veiculo = models.CharField(
+        verbose_name="Placa do Veículo",
+        max_length=7,
+        blank=True,
+        null=True,
+    )
+
+    horario_chegada = models.DateTimeField(
+        verbose_name="Horario de entrada na portaria",
+        auto_now_add=True,
+    )
+
+    horario_saida = models.DateTimeField(
+        verbose_name="Horario de saída na portaria",
+        auto_now_add=False,
+        blank=True,
+        null=True,
+    )
+
+    horario_autorizacao = models.DateTimeField(
+        verbose_name="Horario de autorização de entrada",
+        auto_now = False,
+        blank = True,
+        null= True,
+    )
+
+    morador_responsavel = models.CharField(
+        verbose_name="Morador responsável pela autorização",
+        max_length=128,
+        blank=True,
+    )
+
+    registrado_por = models.ForeignKey(
+        "porteiros.Porteiro",
+        verbose_name="Porteiro responsável pelo registro",
+        on_delete= models.PROTECT,
+        )
+    
+    class Meta:
+        verbose_name = "Visitante"
+        verbose_name_plural = "Visitantes"
+        db_table = "visitante"
+    
+    def __str__(self):
+        return self.nome_completo
