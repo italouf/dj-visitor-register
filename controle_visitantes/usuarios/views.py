f96410161ac2d99062ae91ae3825ba96ce584983
Italo <ItaloSilva.thera@suzano.com.br>
2023-01-27 10:44:57 -0300

    dados visitantes no template

--- controle_visitantes/usuarios/views.py
+++ controle_visitantes/usuarios/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from visitantes.models import Visitante
 
 def index(request):
 
+    todos_visitantes = Visitante.objects.all()
+
     context = {
         "nome_pagina": "Dashboard Inicial",
+        "todos_visitantes": todos_visitantes,
     }
 
     return render(request, "index.html", context)
